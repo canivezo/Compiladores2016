c834c13e2a63d5f96a7bdb690d8504aba1ea1527
Rubens <Rubens@War-Machine>
2016-08-17 18:55:03 -0300

    Construção da pilha

---
+++ Projeto-Compiladores/src/projeto/compiladores/Pilha.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projeto.compiladores;
+
+/**
+ *
+ * @author Rubens
+ */
+public class Pilha 
+{
+    
+    public Object[] pilha;
+    public int posicao;
+    
+    public Pilha()
+    {
+        this.posicao = -1;
+        this.pilha = new Object[1000];
+        
+    }
+    
+    public boolean pilhaVazia()
+    {
+        if(this.posicao < 0) // Pilha vazia
+        {
+            return true;
+        }
+        else
+            return false;
+    }
+}
