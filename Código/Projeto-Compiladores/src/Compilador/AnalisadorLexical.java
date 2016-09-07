4e3cc90d171932589029f944df9af33c177995c8
lucascarvalhoroncoroni <lucascarvalhoroncoroni@gmail.com>
2016-09-07 16:42:50 -0300

    trataidentificador e palavra reservada

--- Código/Projeto-Compiladores/src/Compilador/AnalisadorLexical.java
+++ Código/Projeto-Compiladores/src/Compilador/AnalisadorLexical.java
@@ -12,34 +12,62 @@ import java.util.Vector;
  * @author lucas
  */
 public class AnalisadorLexical {
-    Vector vetorDeTokens = null;
+    Vector<Token> vetorDeTokens;
     private int linha = 1;
-    
+    private int aux = 0;
+    //leitor de arquivo
     public AnalisadorLexical(String arquivo)
     {
+        vetorDeTokens = new Vector<Token>();
         for(int i = 0; i < arquivo.length(); i++)
         {
-            char caracterLido = leiaCaracter(arquivo,i);
+            char caracterLido = leiaCaracter(arquivo);
             if(caracterLido == '{')
             {
-                while(arquivo.charAt(i) != '}') i++;
+                while(caracterLido != '}' )
+                {
+                    if(caracterLido == '\n' || caracterLido == '\r') linha++;
+                }
                 continue;
             }
             if(Character.isWhitespace(arquivo.charAt(i)))
             {
-                if(arquivo.charAt(i) == '\n' || arquivo.charAt(i) == '\r') linha++;
+                if(caracterLido == '\n' || caracterLido == '\r') linha++;
                 continue;
             }
-            pegaToken(arquivo);
+            pegaToken(arquivo, caracterLido);
         }
     }
     
-    public Token pegaToken(String arquivo)
+    public boolean isfimDoArquivo(String arquivo)
     {
-        Token token = null;
-        //leia caracter
-        
-        return token;
+        if(arquivo.length() == aux) return true;
+        return false;
+    }
+    
+    public Token pegaToken(String arquivo, char caracterLido) throws Exception
+    {
+        if(Character.isDigit(caracterLido))
+        {
+            return trataDigito(arquivo, caracterLido);
+        }
+        if(Character.isAlphabetic(caracterLido))
+        {
+            return trataIdentificadorEPalavraReservada(arquivo, caracterLido);
+        }
+        if(caracterLido ==  ':')
+        {
+            return trataAtribuicao(arquivo, caracterLido);
+        }
+        if(caracterLido == '+' || caracterLido == '-' || caracterLido == '*')
+        {
+            return trataOperadorAritmetico(arquivo, caracterLido);
+        }
+        if(caracterLido == '+' || caracterLido == '-' || caracterLido == '*')
+        {
+            return trataOperadorRelacional(arquivo, caracterLido);
+        }
+        return trataPontuacao(arquivo, caracterLido);
     }
     
     public Token pegaToken()
@@ -50,38 +78,79 @@ public class AnalisadorLexical {
         return token;
     }
     
-    private char leiaCaracter(String arquivo, int count)
+    private char leiaCaracter(String arquivo)
     {
-        return arquivo.charAt(count);
+        aux++;
+        return arquivo.charAt((aux - 1));
     }
     
-    private Token trataDigito()
+    private Token trataDigito(String arquivo, char caracterLido)
     {
         Token token = null;
         return token;
     }
     
-    private Token trataIdentificadorEPalavraReservada()
+    private Token trataIdentificadorEPalavraReservada(String arquivo, char caracterLido) throws Exception
+    {
+        String id;
+        do
+        {
+             id = ""+caracterLido;
+             caracterLido = leiaCaracter(arquivo);
+        }
+        while(Character.isDigit(caracterLido));
+        switch(id)
+        {
+            case "programa":
+                return new Token("sprograma", id, linha);
+            case "se":
+                return new Token("sse", id, linha);
+            case "entao":
+                return new Token("sentao", id, linha);
+            case "senao":
+                return new Token("ssenao", id, linha);
+            case "enquanto":
+                return new Token("senquanto", id, linha);
+            case "faca":
+                return new Token("sfaca", id, linha);
+            case "incio":
+                return new Token("sinício", id, linha);
+            case "fim":
+                return new Token("sfim", id, linha);
+            case "escreva":
+                return new Token("sescreva", id, linha);
+            case "leia":
+                return new Token("sleia", id, linha);
+            case "var":
+                return new Token("svar", id, linha);
+            case "inteiro":
+                return new Token("sinteiro", id, linha);
+            case "booleano":
+                return new Token("sbooleano", id, linha);
+            case "verdadeiro":
+                return new Token("sverdadeiro", id, linha);
+            case "falso":
+                return new Token("sfalso", id, linha);
+        }
+        return null;
+    }
+    
+    private Token trataAtribuicao(String arquivo, char caracterLido)
     {
         return null;
     }
     
-    private Token trataAtribuicao()
+    private Token trataOperadorAritmetico(String arquivo, char caracterLido)
     {
         return null;
     }
     
-    private Token trataOperadorAritmetico()
+    private Token trataOperadorRelacional(String arquivo, char caracterLido)
     {
         return null;
     }
     
-    private Token trataOperadorRelacional()
-    {
-        return null;
-    }
-    
-    private Token trataPontuacao()
+    private Token trataPontuacao(String arquivo, char caracterLido)
     {
         return null;
     }
